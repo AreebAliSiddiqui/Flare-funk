ba15c6430b0d538a9682800e342cf0060ae3bb67
AreebAliSiddiqui <areebalisiddiqui30@gmail.com>
2026-06-24 03:19:25 +0500

    updated

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ spaceBetween: 30,
 
 loop: true,
 
+speed:500,
+
 navigation: {
 nextEl: ".swiper-button-next",
 prevEl: ".swiper-button-prev",
@@ -27,6 +29,44 @@ slidesPerView: 3,
 
 1024: {
 slidesPerView: 4,
+}
+
+}
+
+});
+
+
+// team members
+
+const teamSwiper = new Swiper(".teamSwiper", {
+
+slidesPerView:4,
+
+spaceBetween:30,
+
+loop:true,
+
+grabCursor:true,
+
+speed:500,
+
+navigation:{
+nextEl:".team-next",
+prevEl:".team-prev",
+},
+
+breakpoints:{
+
+320:{
+slidesPerView:1,
+},
+
+768:{
+slidesPerView:2,
+},
+
+1024:{
+slidesPerView:4,
 }
 
 }
